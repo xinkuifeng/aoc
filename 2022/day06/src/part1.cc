7d67697b43309d9df9d17a4982ac3000ed715cc2
Xinkui FENG <xi.feng@criteo.com>
2022-12-27 23:23:28 +0100

    Add the solutions of 2022/12/06

---
+++ 2022/day06/src/part1.cc
@@ -0,0 +1,57 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solve(string line) {
+    vector<int> table(26, -1);
+
+    int start = 0;
+    int cnt = 0;
+    for (int i = 0; i < line.size(); ++i) {
+        char c = line[i];
+        int idx = c - 'a';
+        // this char is repeated
+        if (table[idx] >= start) {
+            // not good => restart
+            start = table[idx] + 1;
+            cnt = i - start;
+        }
+        table[idx] = i;
+        ++cnt;
+        if (cnt == 4) {
+            break;
+        }
+    }
+    return start + cnt;
+}
+
+int main(int argc, char ** argv)
+{
+    cout << "Hello, World!" << endl;
+
+    if (argc != 2) {
+        cout << "usage: ./a.out FILE_NAME" << endl;
+        return -1;
+    }
+
+    ifstream file(argv[1]);
+    if (!file) {
+        cout << "failed to open " << argv[1] << endl;
+        return -1;
+    }
+
+    while (!file.eof()) {
+        string line;
+        while (getline(file, line)) {
+            if (line.empty()) break;
+
+            int pos = solve(line);
+            cout << line << " -> " << pos << endl;
+        }
+    }
+
+    return 0;
+}
